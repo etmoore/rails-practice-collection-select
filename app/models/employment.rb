4c81062fe211b215b20b792355c4a4a020f7eada
Evan Moore <etmoore@gmail.com>
2014-11-18 12:01:29 -0700

    added validations adn error messages

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -1,4 +1,6 @@
 class Employment < ActiveRecord::Base
+  validates :job_title, :location, :organization, :person, presence: true
+
   belongs_to :job_title
   belongs_to :location
   belongs_to :organization
